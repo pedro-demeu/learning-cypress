d357bd9b269fc86f363b3f2a0d045fcf2882ab1d
pdemeu <pdemeu@outlook.com>
2023-06-06 13:36:23 -0300

    feat: implement two new commands to run test

--- cypress-basico-v2/cypress/integration/CAC-TAT.spec.js
+++ cypress-basico-v2/cypress/integration/CAC-TAT.spec.js
@@ -154,5 +154,23 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     cy.get('#file-upload')
     .selectFile('@sampleFile')
     .should(($el) => expect($el[0].files[0].name).to.eq('example.json'))
-  })
+  });
+
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function(){
+    cy.get('#privacy a')
+      .should('have.attr', 'target', '_blank');
+  });
+
+  it('acessa a página da política de privacidade removendo o target e então clicando no link', function(){
+    cy.get('#privacy a')
+      .invoke('removeAttr', 'target')
+      .click();
+  });
+
+  it('testa a página da política de privacidade de forma independente', function(){
+    cy.visit('./src/privacy.html');
+    cy.contains('Política de privacidade')
+      .should('be.visible');
+  });
+
 })
